fb8d7467b07274cda5fa8fd554a7c89c9953467f
akishen@uci.edu <akishen@uci.edu>
2020-02-03 22:47:38 -0800

    Initial Commit

---
+++ InaijsaBettsMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InaijsaBettsMusic
+//
+//  Created by Adit Kishen on 2/3/20.
+//  Copyright © 2020 Adit Kishen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
